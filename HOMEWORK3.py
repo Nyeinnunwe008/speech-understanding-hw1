70d8e1a80a0107b52d36555ba0579e17e80a56e2
nyeinnunwe <nyeinnunwe8@gmail.com>
2026-05-05 22:29:44 +0900

    homework3 solution

--- HOMEWORK3.py
+++ HOMEWORK3.py
@@ -1,30 +1,6 @@
-"""
-Homework 3
 
-This file defines the function 'words2characters'.
-
-To see documentation, use:
-help(homework3.words2characters)
-"""
 
 def words2characters(words):
-    """
-    This function converts a list of words into a list of characters.
-
-    @param:
-    words - a list of words
-
-    @return:
-    characters - a list of characters
-
-    Every element of "words" is converted to a string, then split into
-    characters, each of which is appended to the result list.
-
-    Example:
-    words = ['hello', 1.234, True]
-    returns:
-    ['h', 'e', 'l', 'l', 'o', '1', '.', '2', '3', '4', 'T', 'r', 'u', 'e']
-    """
 
     characters = []
 
@@ -33,9 +9,6 @@ def words2characters(words):
             characters.append(ch)
 
     return characters
-
-
-# Optional test (remove if your teacher doesn't want extra output)
 if __name__ == "__main__":
     sample = ['hello', 1.234, True]
     print(words2characters(sample))

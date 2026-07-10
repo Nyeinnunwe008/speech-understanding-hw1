3b4355ed078c26fc64699a3be1d9fd40570253bf
nyeinnunwe <nyeinnunwe8@gmail.com>
2026-07-10 14:59:57 +0900

    homework11 solution

---
+++ HOMEWORK11.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+
+def transcribe_wavefile(filename, language):
+    '''
+    Use sr.Recognizer.AudioFile(filename) as the source,
+    recognize from that source,
+    and return the recognized text.
+
+    @params:
+    filename (str) - the filename from which to read the audio
+    language (str) - the language of the audio
+
+    @returns:
+    text (str) - the recognized speech
+    '''
+    recognizer = sr.Recognizer()
+
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+
+    try:
+        text = recognizer.recognize_google(audio, language=language)
+    except sr.UnknownValueError:
+        text = ""
+    except sr.RequestError:
+        text = ""
+
+    return text
